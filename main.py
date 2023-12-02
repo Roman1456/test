e2c0b8686be40f00d79ef146d7c67037aa1bcc78
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2023-12-02 19:59:12 +0300

    test 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 app = QApplication([])
 window = QWidget ()
 window.resize(500,500)
-lbl = QLabel("Скільки буде 2+2")
+lbl = QLabel("Скільки буде 2+2?")
 ans1=QRadioButton("4")
 ans2=QRadioButton("14")
 ans3=QRadioButton("1")
@@ -36,6 +36,47 @@ ans1.clicked.connect(true_var_1)
 ans2.clicked.connect(false_var_2)
 ans3.clicked.connect(false_var_2)
 ans4.clicked.connect(false_var_2)
+
+
+lbl1 =QLabel("Найвища гора в України?")
+ans1=QRadioButton("Мусала")
+ans2=QRadioButton("Говерла")
+ans3=QRadioButton("Роман-Кіш")
+ans4=QRadioButton("Карпати")
+
+
+
+mine_line.addWidget(lbl1)
+
+h1 = QHBoxLayout()
+h2 = QHBoxLayout()
+h1.addWidget(ans1)
+h1.addWidget(ans2)
+h2.addWidget(ans3)
+h2.addWidget(ans4)
+
+mine_line.addLayout(h1)
+mine_line.addLayout(h2)
+window.setLayout(mine_line)
+
+def tru_var_3():
+    msg = QMessageBox()
+    msg.setText("Павильно")
+    msg.exec()
+
+def false_var_4():
+    msg = QMessageBox()
+    msg.setText("Неправильно")
+    msg.exec()
+
+ans1.clicked.connect(false_var_4)
+ans2.clicked.connect(tru_var_3)
+ans3.clicked.connect(false_var_4)
+ans4.clicked.connect(false_var_4)
+
+
+
+
 
 window.show()
 app.exec()
